af85e8590015228ddab5658a7765c758fdb0b09f
njm1250 <njm1250@gmail.com>
2024-08-13 21:59:05 +0900

    Reddit API 테스트

--- sentiment/urls.py
+++ sentiment/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),  # 기본 페이지 URL
+    path('reddit', views.get_recent_reddit_posts, name='reddit'),
 ]
--- sentiment/views.py
+++ sentiment/views.py
@@ -1,6 +1,35 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer # type: ignore
+from datetime import datetime, timedelta
+from decouple import config
+import praw
+
+def get_recent_reddit_posts(request):
+    client_id = config('REDDIT_CLIENT_ID')  
+    client_secret = config('REDDIT_SECRET')  
+    user_agent = config('REDDIT_USER_AGENT')  
+
+    reddit = praw.Reddit(
+        client_id=client_id,
+        client_secret=client_secret,
+        user_agent=user_agent
+    )
+
+    # 서브레딧
+    subreddit = reddit.subreddit('cryptocurrency')
+    query = "xrp"  
+
+    posts = []
+    for submission in subreddit.search(query, limit=10, time_filter='day'):
+        post_time = datetime.utcfromtimestamp(submission.created_utc)
+        posts.append({
+            'title': submission.title,
+            'selftext': submission.selftext,
+            'created': post_time.strftime('%Y-%m-%d %H:%M:%S'),
+        })
+
+    return JsonResponse({'posts': posts})
 
 def index(request):
     # 분석할 문장
