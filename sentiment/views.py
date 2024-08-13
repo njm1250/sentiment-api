b489fb587405636c3ed4f510239a78269e1cc41f
njm1250 <njm1250@gmail.com>
2024-08-13 20:46:04 +0900

    vaderSentiment 감정분석 기능 테스트

--- sentiment/views.py
+++ sentiment/views.py
@@ -1,5 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer # type: ignore
 
 def index(request):
-    return HttpResponse("Hello, World!")
+    # 분석할 문장
+    sentence = "Yo I'm so Excited about the Tesla right now."
+
+    analyzer = SentimentIntensityAnalyzer()
+    vs = analyzer.polarity_scores(sentence)
+
+    if vs['compound'] > 0:
+        sentiment = 'Positive'
+    elif vs['compound'] == 0:
+        sentiment = 'Neutral'
+    else:
+        sentiment = 'Negative'
+
+    print(f"Compound Score: {vs['compound']}")
+
+    # 결과를 HTTP 응답으로 반환
+    return HttpResponse(sentiment)
