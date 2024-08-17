b8ebf9a271cf3f3a1ad2acaf19f93f679b7f5059
njm1250 <njm1250@gmail.com>
2024-08-17 21:50:58 +0900

    mysql 연동 및 subreddits 테이블 추가

--- market_mood_tracker/settings.py
+++ market_mood_tracker/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'market_mood_tracker.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'marketmood',  
+        'USER': 'root',      
+        'PASSWORD': '123456', 
+        'HOST': 'localhost',   
+        'PORT': '3306',         
     }
 }
 
--- sentiment/models.py
+++ sentiment/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Subreddits(models.Model):
+    subreddit_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+    class Meta:
+        db_table = 'subreddits'
