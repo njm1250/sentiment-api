d1a0aa39a44f237b2e86c128d5f69cfb9836949b
njm1250 <njm1250@gmail.com>
2024-09-07 22:58:36 +0900

    mysqlclient

--- sentiment/tests.py
+++ sentiment/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from sentiment import services
 
-# Create your tests here.
+class SentimentTest(TestCase):
+    def test_sentiment(self):
+        posts = services.get_recent_reddit_posts('stocks', 'tesla', 100, 'day')
+        avg_sentiment = services.calculate_avg_sentiment(posts)
+
+        ans = ''
+        for post in posts:
+            ans += post['selftext']
+        print("avg score: " + str(avg_sentiment) + ans)
+
--- sentiment/views.py
+++ sentiment/views.py
@@ -3,7 +3,11 @@ from django.http import HttpResponse
 from . import services
 
 def index(request):
-    posts = services.get_recent_reddit_posts('cryptocurrency', 'xrp', 100, 'day')
+    posts = services.get_recent_reddit_posts('cryptocurrency', 'btc', 100, 'day')
     avg_sentiment = services.calculate_avg_sentiment(posts)
+
+    ans = ''
+    for post in posts:
+        ans += post['content']
     
-    return HttpResponse('Avg Score: ' + str(avg_sentiment))
+    return HttpResponse('Avg Score: ' + str(avg_sentiment) + ans)
